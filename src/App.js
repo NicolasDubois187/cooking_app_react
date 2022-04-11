e7210db3621fa6d09a2e59196bcc958c9dbdf7c9
NicolasDubois187 <nicolasdubois187.pro@gmail.com>
2022-04-11 12:21:21 +0200

    cooking_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cooking-app</h1>
+      {/* //lien sur : themealDB (tester le lien) 
+      //créer variable pour stocker les recettes (useState)
+      //Aller chercher les données avec axios via le
+      useEffect puis les stocker.
+      //Afficher les données à l'écran (Nom, origine, photo, lien de la video)
+      // Rechercher un repas via un input, filter
+      // BONUS
+        // proposer des boutons avec spécialités de certains pays (french, itailan, spanish)
+       */}
     </div>
   );
-}
+};
 
 export default App;
